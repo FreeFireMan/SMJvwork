49c3c546299c25a731e9a674b1d531f0a34b1b93
aonyshchuk <aonyshchuk@apple.com>
2019-08-23 10:38:18 -0700

    collect filter config when fetching the catalog

--- src/main/java/com/example/demo/db/model/ModelHolder.java
+++ src/main/java/com/example/demo/db/model/ModelHolder.java
@@ -3,5 +3,6 @@ package com.example.demo.db.model;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public interface ModelHolder {
+
     ObjectNode getValue();
 }
--- src/main/java/com/example/demo/db/model/filterConfig/FilterGroup.java
+++ src/main/java/com/example/demo/db/model/filterConfig/FilterGroup.java
@@ -1,19 +1,44 @@
 package com.example.demo.db.model.filterConfig;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.*;
+import java.util.*;
+import static com.example.demo.utils.OptionalUtils.*;
 
-import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FilterGroup {
-    private Integer id;
-    private String name;
-    private Map<Integer,FilterAttribute> attributes;
 
+    private int id;
+
+    private String name;
+
+    public FilterGroup(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    private Map<Integer, FilterAttribute> attributes;
+
+    public void merge(ObjectNode node) {
+        optInt(node, "id").ifPresent(id -> {
+            optStr(node, "name").ifPresent(name -> {
+                optNode(node, "values", JsonNode::isArray).ifPresent(values -> {
+                    if (attributes == null) attributes = new HashMap<>();
+
+                    FilterAttribute attr = attributes.getOrDefault(id, new FilterAttribute(id, name, optStr(node, "unit")));
+                    values.forEach(val -> {
+                        if (val.isObject()) {
+                            attr.merge((ObjectNode) val);
+                        }
+                    });
+                });
+            });
+        });
+    }
 }

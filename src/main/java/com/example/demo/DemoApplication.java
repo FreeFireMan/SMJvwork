d70877fd432517da9614c0175f49b145ccc949b9
aonyshchuk <aonyshchuk@apple.com>
2019-09-03 00:47:06 -0700

    fix filtering

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,19 @@
 package com.example.demo;
 
 import com.example.demo.service.catalog.CatalogService;
+import com.example.demo.service.product.ProductService;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.servlet.MultipartConfigElement;
+import java.util.Optional;
 
 @EnableScheduling
 @SpringBootApplication
@@ -16,8 +21,15 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+        ProductService ps = ctx.getBean(ProductService.class);
 
-
+//        JsonNodeFactory f = JsonNodeFactory.instance;
+//        ObjectNode query = f.objectNode();
+//        query.putArray("Тип").add("Пластиковый");
+//
+//        Page<ObjectNode> page = ps.findShortDescriptions(0, 20, 70038, query, Optional.empty());
+//        System.out.println(page.getTotalElements());
+//        page.getContent().forEach(c -> System.out.println(c));
 
     }
 }

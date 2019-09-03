f07a99f55a3bed6246519941afacef7e8d93febd
aonyshchuk <aonyshchuk@apple.com>
2019-09-03 12:10:43 -0700

    fix multi filtering

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.servlet.MultipartConfigElement;
@@ -23,13 +24,15 @@ public class DemoApplication {
         ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
         ProductService ps = ctx.getBean(ProductService.class);
 
-//        JsonNodeFactory f = JsonNodeFactory.instance;
-//        ObjectNode query = f.objectNode();
-//        query.putArray("Тип").add("Пластиковый");
-//
-//        Page<ObjectNode> page = ps.findShortDescriptions(0, 20, 70038, query, Optional.empty());
-//        System.out.println(page.getTotalElements());
-//        page.getContent().forEach(c -> System.out.println(c));
+        JsonNodeFactory f = JsonNodeFactory.instance;
+        ObjectNode query = f.objectNode();
+        query.putArray("Возраст от").add("8").add("9").add("7");
+        query.putArray("Content status").add("Available");
+        query.putArray("Product Category").add("Standard Retail");
+
+        Page<ObjectNode> page = ps.findShortDescriptions(0, 20, 70038, query, Sort.unsorted());
+        System.out.println(page.getTotalElements());
+        page.getContent().forEach(c -> System.out.println(c));
 
     }
 }
